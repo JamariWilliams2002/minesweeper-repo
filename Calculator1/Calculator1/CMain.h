ed8d203d61e735903f3f8a4f975aa0f13d8ca983
JamariWilliams2002 <jamwilliams02@gmail.com>
2022-04-19 22:47:18 -0400

    commit

--- Calculator1/Calculator1/CMain.h
+++ Calculator1/Calculator1/CMain.h
@@ -94,6 +94,7 @@ private:
 	void OnClickMisc(wxCommandEvent &evt);
 	//helper methods
 	void ResetArithmetic();
+	void ResetPrevAndNextNum();
 	int ConvertToBinary(int decimal);
 	int BinaryToDecimal(int n);
 	std::string DecimalToHex(int num);

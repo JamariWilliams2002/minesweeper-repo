0721c739e7df300b73bcf1a31ad465098de7f708
JamariWilliams2002 <jamwilliams02@gmail.com>
2022-04-20 18:18:02 -0400

    last commit

--- calculator1/Calculator1/CMain.h
+++ calculator1/Calculator1/CMain.h
@@ -33,6 +33,8 @@ private:
 	//doesn't include P or E
 	std::vector<wxString> operationsInPemdas { "X", "/", "%", "+", "-" };
 
+	//singleton classes
+
 	//flags for operations
 	//means atleast one arithmetic has been clicked and we are working on a second number
 	bool onNextNum = false;
@@ -60,7 +62,7 @@ private:
 	wxSize displaySize;
 	wxString prePreviewStr = "";
 	//copy of prepreviewstr, just in the number's original form
-	wxString prePreviewDecStr = "";
+	wxString previewDecStr = "";
 	//keep a copy of the string
 	wxString cpy;
 

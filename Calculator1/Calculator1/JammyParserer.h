62b55b95268ecbecd8360ae9352d3a51f304eea3
JamariWilliams2002 <jamwilliams02@gmail.com>
2022-04-28 21:22:33 -0400

    before functions

--- Calculator1/Calculator1/JammyParserer.h
+++ Calculator1/Calculator1/JammyParserer.h
@@ -7,29 +7,16 @@ class JammyParser
 public:
 	JammyParser();
 	~JammyParser();
-	int Interpret(std::string expression);
-	int Interpret(wxString expression);
+	int Interpret(const char* equation);
 private:
-	std::vector<double> nums;
-	std::vector<char> operations;
-	std::vector<int> operationPrecedence;
-	//helper variables
-	int prec1;
-	int prec2;
+    const char* expressionToParse;
 
-	bool IsOperator(char c);
-	bool IsNumber(char c);
-	int GetPrecedence(char c);
-	int GetNumberFromChar(char c);
-	void OrganizeVectors();
-	void SwapOperators(int index);
-	void SwapNumbers(int index);
-	int Evaluate(int num1, int num2, char op);
-
-	std::vector<char>::iterator CheckMultiply();
-	std::vector<char>::iterator CheckDivide();
-	std::vector<char>::iterator CheckModulus();
-
+    char peek();
+    char get();
+    int expression();
+    int number();
+    int factor();
+    int term();
 
 };
 
